b468454cae3d89609d9f8304ed1816bb074973fa
Gai lowvapong <0kaiyast0@gmail.com>
2017-04-20 00:43:53 +0700

    Inti

---
+++ bar.php
@@ -0,0 +1,38 @@
+          <!-- start:Left Menu -->
+            <div id="left-menu">
+              <div class="sub-left-menu scroll">
+                <ul class="nav nav-list">
+                    <li><div class="left-bg"></div></li>
+                    <li data-value="#" class="time">
+                      <h1 class="animated fadeInLeft">21:00</h1>
+                      <p class="animated fadeInRight">Sat,October 1st 2029</p>
+                    </li>
+                    <li data-value="optimize.php" class="ripple">
+                      <a  class="tree-toggle nav-header"><span class="fa-wrench  fa"></span> Optimize 
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                    <li data-value="worker.php"  class="ripple">
+                      <a class="tree-toggle nav-header"><span class="fa-users fa"></span> Worker 
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                    <li data-value="product.php" class="ripple">
+                      <a class="tree-toggle nav-header">
+                       <span  class="fa fa-shopping-cart " href="index.php" ></span> Product
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                    <li data-value="chart.php" class="ripple">
+                      <a class="tree-toggle nav-header" href="index.php">
+                        <span  class="fa-area-chart fa"></span> Chart
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                  
+                    <li data-value="index.php" ><a href="index.php" >Logout</a></li>
+                  </ul>
+                </div>
+            </div>
+          <!-- end: Left Menu -->
+		  
---
+++ worker.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+<?php include('header.php'); ?>
+	
+  </head>
+
+ <body id="mimin" class="dashboard">
+      <!-- start: Header -->
+        <nav class="navbar navbar-default header navbar-fixed-top">
+          <div class="col-md-12 nav-wrapper">
+            <div class="navbar-header" style="width:100%;">
+              <div class="opener-left-menu is-open">
+                <span class="top"></span>
+                <span class="middle"></span>
+                <span class="bottom"></span>
+              </div>
+                <a href="home.php" class="navbar-brand"> 
+                 <b>iFactory</b>
+                </a>
+
+              <ul class="nav navbar-nav search-nav">
+                <li>
+
+                </li>
+              </ul>
+
+
+            </div>
+          </div>
+        </nav>
+      <!-- end: Header -->
+
+      <div class="container-fluid mimin-wrapper">
+  
+<?php include('bar.php'); ?>
+
+  		
+          <!-- start: content -->
+            <div id="content">
+
+                <div class="panel">
+                  <div class="panel-body">
+                      <div class="col-md-12 col-sm-12">
+                        <h3 class="animated fadeInLeft">Optimize Service</h3>
+                         <div class="right">
+                              <input type="button" class="btn btn-raised  btn-success" value="+ Worker"/>                    
+                         </div>
+                    </div>
+                  </div>                    
+                </div>
+
+
+                <div class="col-md-12" >
+
+                         <div class="col-md-12 padding-0">
+						
+							  <div class="col-md-12 top-20 padding-0">
+									
+
+
+                  <div class="col-md-12 top-20">                  
+                    <div class="panel">
+     
+									<div class="panel-heading"><h3>Worker</h3></div>
+									<div class="panel-body">
+									<div class="responsive-table">
+										<table id="datatables-example" class="table table-striped table-bordered" width="100%" cellspacing="0">
+										  <thead>
+											<tr>
+											  <th>ID</th>
+											  <th>Name</th>
+											</tr>
+										  </thead>
+										  <tbody>
+											<tr>
+											  <td>Tiger Nixon</td>
+											  <td>System Architect</td>
+
+											</tr>
+											<tr>
+											  <td>Swim Nixon</td>
+											  <td>System Architect</td>
+
+											</tr>
+										  </tbody>
+											</table>
+										  </div>
+									  </div>
+									  
+									</div>
+								  </div>  
+							  </div>
+           
+
+
+		  
+						</div>
+				</div>
+      		</div>
+          <!-- end: content -->
+
+    
+         
+          
+      </div>
+
+   
+
+<!-- start: Javascript -->
+<script src="asset/js/jquery.min.js"></script>
+<script src="asset/js/jquery.ui.min.js"></script>
+<script src="asset/js/bootstrap.min.js"></script>
+
+
+
+<!-- plugins -->
+<script src="asset/js/plugins/moment.min.js"></script>
+<script src="asset/js/plugins/jquery.datatables.min.js"></script>
+<script src="asset/js/plugins/datatables.bootstrap.min.js"></script>
+<script src="asset/js/plugins/jquery.nicescroll.js"></script>
+
+
+    <!-- custom -->
+     <script src="asset/js/main.js"></script>
+<script type="text/javascript">
+  $(document).ready(function(){
+    $('#datatables-example').DataTable();
+  });
+    
+	 
+	     $(".nav li").on("click", function() {
+			//alert($(this).data('value'));
+			document.location.href = $(this).data('value');
+		});
+	
+   
+</script>
+  <!-- end: Javascript -->
+  </body>
+</html>
